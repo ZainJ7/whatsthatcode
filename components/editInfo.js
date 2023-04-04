4738e82ead59b5f7ae443f98ec612fba834e6cf3
ZainJ7 <zainjanwani@gmail.com>
2023-04-04 11:24:19 +0100

    Refactoring and Error/Success Messages
    Refactoring and error/success messages done for EditInfo page

--- components/editInfo.js
+++ components/editInfo.js
@@ -1,14 +1,6 @@
 import React, { Component } from "react";
-import {
-  View,
-  ScrollView,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-} from "react-native";
+import {View, ScrollView, Text, TextInput, TouchableOpacity, StyleSheet,} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import * as EmailValidator from "email-validator";
 import globalStyles from "../styles/global";
 
 class EditInfo extends Component {
@@ -17,8 +9,7 @@ class EditInfo extends Component {
     last_name: "",
     email: "",
     password: "",
-    error: "",
-    errorForState: "", //none of the errors work
+    message: ""
   };
 
   async componentDidMount() {
@@ -51,77 +42,11 @@ class EditInfo extends Component {
       });
   }
 
-  _onPressButton() {
-    this.setState({ submitted: true });
-    this.setState({ error: "" });
-    this.setState({ success: false });
-
-    if (!(this.state.email && this.state.password)) {
-      this.setState({ errorForState: "Must enter email and password" });
-      return;
-    }
-
-    if (!EmailValidator.validate(this.state.email)) {
-      this.setState({ errorForState: "Must enter valid email" });
-      return;
-    }
-
-    const PASSWORD_REGEX = new RegExp(
-      "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
-    );
-    if (!PASSWORD_REGEX.test(this.state.password)) {
-      this.setState({
-        errorForState: "Password isn't strong enough",
-      });
-      return;
-    }
-
-    if (this.state.password !== this.state.confirmPass) {
-      this.setState({ errorForState: "Passwords do not match" });
-      return;
-    }
-
-    this.setState({ success: true });
-
-    return fetch("http://localhost:3333/api/1.0.0/user", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        first_name: this.state.first_name,
-        last_name: this.state.last_name,
-        email: this.state.email,
-        password: this.state.password,
-      }),
-    })
-      .then((response) => {
-        if (response.status === 201) {
-          return response.json();
-        } else if (response.status === 400) {
-          throw "Failed validation";
-        } else {
-          throw "Something went wrong";
-        }
-      })
-      .then((rJson) => {
-        console.log(rJson);
-        this.setState({ error: "User added successfully" });
-        this.setState({ submitted: false });
-        this.props.navigation.navigate("Login");
-      })
-      .catch((error) => {
-        this.setState({ error: error });
-        this.setState({ submitted: false });
-      });
-  }
 
   editInfo = async () => {
-    // get token and user id from AsyncStorage
     const token = await AsyncStorage.getItem("whatsthat_session_token");
     const userId = await AsyncStorage.getItem("whatsthat_user_id");
 
-    // make PATCH request
     try {
       const response = await fetch(
         `http://localhost:3333/api/1.0.0/user/${userId}`,
@@ -140,26 +65,35 @@ class EditInfo extends Component {
         }
       );
       if (response.status === 200) {
-        this.setState({ error: "User updated successfully" });
+        this.setState({ message: "User Updated Successfully!" }, () => {
+          setTimeout(() => {
+            this.setState({ message: "" });
+          }, 3000);
+        });
       } else if (response.status === 400) {
-        this.setState({ error: "Failed Validation" });
+        this.setState({ message: "Failed Validation" }, () => {
+          setTimeout(() => {
+            this.setState({ message: "" });
+          }, 3000);
+        });
         throw "Failed validation";
       } else {
-        this.setState({ error: "Something went Wrong" });
-        throw "Something went wrong";
+        this.setState({ message: "Something Went Wrong" }, () => {
+          setTimeout(() => {
+            this.setState({ message: "" });
+          }, 3000);
+        });
       }
     } catch (error) {
-      console.log("its fine");
     }
   };
 
   render() {
-    const { navigation } = this.props;
     return (
       <View style={globalStyles.container}>
+        <Text style={globalStyles.title}>Edit User Info</Text>
+        <Text style={styles.message}>{this.state.message}</Text> 
         <ScrollView>
-          <Text style={globalStyles.title}>Edit User Info</Text>
-
           <View style={styles.formItem}>
             <Text style={styles.formLabel}>First Name:</Text>
             <TextInput
@@ -186,13 +120,7 @@ class EditInfo extends Component {
               placeholder=" Enter email..."
               style={styles.formInput}
               onChangeText={(email) => this.setState({ email })}
-              value={this.state.email}
-            />
-            {this.state.errorForState !== "" && (
-              <Text style={styles.errorForState}>
-                {this.state.errorForState}
-              </Text>
-            )}
+              value={this.state.email}/>
           </View>
 
           <View style={styles.formItem}>
@@ -202,13 +130,7 @@ class EditInfo extends Component {
               style={styles.formInput}
               secureTextEntry
               onChangeText={(password) => this.setState({ password })}
-              value={this.state.password}
-            />
-            {this.state.errorForState !== "" && (
-              <Text style={styles.errorForState}>
-                {this.state.errorForState}
-              </Text>
-            )}
+              value={this.state.password}/>
           </View>
 
           <View style={styles.formItem}>
@@ -216,14 +138,7 @@ class EditInfo extends Component {
               <Text style={styles.formTouchText}>Edit Info</Text>
             </TouchableOpacity>
           </View>
-
-          {this.state.submitted && !this.state.error && (
-            <Text style={styles.success}>Success! Account Created</Text>
-          )}
           <>
-            {this.state.error && (
-              <Text style={styles.error}>{this.state.error}</Text>
-            )}
           </>
         </ScrollView>
       </View>
@@ -263,29 +178,10 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 20,
   },
-  error: {
-    color: "red",
+  message: {
     fontSize: 20,
-    marginTop: 5,
-    marginBottom: 10,
+    alignSelf: "stretch",
     textAlign: "center",
-  },
-  success: {
-    color: "green",
-    fontSize: 20,
-    marginTop: 5,
-    marginBottom: 10,
-    textAlign: "center",
-  },
-  dontHaveAccountContainer: {
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 20,
-  },
-  dontHaveAccountButton: {
-    color: "black",
-    fontSize: 25,
-    textDecorationLine: "underline",
   },
 });
 
